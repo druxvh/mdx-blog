34693275efd346803aa7a32dfe04ad5d82d4cfab
dm <druavhm@gmail.com>
2024-12-18 18:19:16 +0530

    refactor: added blog and contact links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,11 +5,10 @@ import { buttonVariants } from "@/components/ui/button";
 import { ModeToggle } from "./theme-toggle";
 import HamburgerMenu from "./Hamburger-Menu";
 
-
 function Navbar() {
   return (
     <nav className="h-16 flex justify-between bg-background/60 sticky top-0 border-b px-8 backdrop-blur">
-      <div className="flex items-center text-xl font-bold">drx</div>
+      <div className="flex items-center text-xl font-mono font-bold">drx</div>
 
       <div className="flex items-center gap-4">
         <ul className="hidden md:flex items-center gap-4">
@@ -17,7 +16,13 @@ function Navbar() {
             <Link href={"/"}>Home</Link>
           </li>
           <li>
+            <Link href={"/blog"}>Blog</Link>
+          </li>
+          <li>
             <Link href={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link href={"/contact"}>Contact</Link>
           </li>
           <li>
             <Link href={"/"} className={buttonVariants({ variant: "outline" })}>
@@ -31,7 +36,7 @@ function Navbar() {
           </li>
         </ul>
         <ModeToggle />
-        <HamburgerMenu className="flex md:hidden " />
+        <HamburgerMenu className="flex md:hidden" />
       </div>
     </nav>
   );
